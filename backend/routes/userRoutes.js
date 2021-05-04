098103d58104d4f1c8d7c1cf93ea2c62b5a3e171
Maksim Murza <maksim.murza.v@gmail.com>
2021-05-05 01:29:52 +0300

    backend of creating new user

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,9 +1,14 @@
 import express from "express";
-import { authUsers, getUserProfile } from "../controllers/userController.js";
+import {
+	authUsers,
+	registerUser,
+	getUserProfile,
+} from "../controllers/userController.js";
 import { protect } from "../middleware/authMiddleware.js";
 const routes = express.Router();
 
 routes.post("/login", authUsers);
+routes.route("/").post(registerUser);
 routes.route("/profile").get(protect, getUserProfile);
 
 export default routes;
