85a4975d0a834541184b91ca34703ef174810869
Maksim Murza <maksim.murza.v@gmail.com>
2021-05-07 16:16:15 +0300

    user update backend

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -44,7 +44,7 @@ const registerUser = asyncHandler(async (req, res) => {
 			token: generateToken(user._id),
 		});
 	} else {
-		res.status(400);
+		res.status(401);
 		throw new Error("Invalid user data");
 	}
 });
@@ -59,9 +59,32 @@ const getUserProfile = asyncHandler(async (req, res) => {
 			isAdmin: user.isAdmin,
 		});
 	} else {
-		res.status(401);
+		res.status(404);
 		throw new Error("User not found");
 	}
 });
 
-export { authUsers, registerUser, getUserProfile };
+const updateUserProfile = asyncHandler(async (req, res) => {
+	const user = await User.findById(req.user._id);
+	if (user) {
+		user.name = req.body.name || user.name;
+		user.email = req.body.email || user.email;
+		// to avoid different hashes
+		if (req.body.password) {
+			user.password = req.body.password;
+		}
+		const updatedUser = await user.save();
+		res.json({
+			_id: updatedUser._id,
+			name: updatedUser.name,
+			email: updatedUser.email,
+			isAdmin: updatedUser.isAdmin,
+			token: generateToken(updatedUser._id),
+		});
+	} else {
+		res.status(404);
+		throw new Error("User not found");
+	}
+});
+
+export { authUsers, registerUser, getUserProfile, updateUserProfile };
--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -3,12 +3,16 @@ import {
 	authUsers,
 	registerUser,
 	getUserProfile,
+	updateUserProfile,
 } from "../controllers/userController.js";
 import { protect } from "../middleware/authMiddleware.js";
 const routes = express.Router();
 
 routes.post("/login", authUsers);
 routes.route("/").post(registerUser);
-routes.route("/profile").get(protect, getUserProfile);
+routes
+	.route("/profile")
+	.get(protect, getUserProfile)
+	.put(protect, updateUserProfile);
 
 export default routes;
