1029133722025d53a81f051c28ea7ceea86ec1ed
Maksim Murza <maksim.murza.v@gmail.com>
2021-05-19 15:25:39 +0300

    product delete backend

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -15,4 +15,20 @@ const getProductById = asyncHandler(async (req, res) => {
 	res.json(product);
 });
 
-export { getProducts, getProductById };
+const deleteProduct = asyncHandler(async (req, res) => {
+	const product = await Product.findById(req.params.id);
+
+	if (product) {
+		await product.remove().catch((error) => {
+			throw new Error("Error while removing");
+		});
+		res.json({ message: "Product removed" });
+	} else {
+		res.status(404);
+		throw new Error("Product not found");
+	}
+
+	res.json(product);
+});
+
+export { getProducts, getProductById, deleteProduct };
--- backend/routes/productsRoutes.js
+++ backend/routes/productsRoutes.js
@@ -2,11 +2,13 @@ import express from "express";
 import {
 	getProducts,
 	getProductById,
+	deleteProduct,
 } from "../controllers/productController.js";
+import { protect, admin } from "../middleware/authMiddleware.js";
 
 const routes = express.Router();
 
 routes.route("/").get(getProducts);
-routes.route("/:id").get(getProductById);
+routes.route("/:id").get(getProductById).delete(protect, admin, deleteProduct);
 
 export default routes;
